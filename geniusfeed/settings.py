06590d1fcea2e3090893d49500507226e38f4e28
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2015-06-18 08:06:57 +0000

    Add configuration for postgreSQL database

--- geniusfeed/settings.py
+++ geniusfeed/settings.py
@@ -68,8 +68,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db-drf',
+        'USER': 'drest',
+        'PASSWORD': 'drestpass123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
