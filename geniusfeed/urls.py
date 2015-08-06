73feecd30643b8e56476b8e84e01a9e716b92092
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2015-08-06 18:37:19 +0000

    Add viewsets

--- geniusfeed/urls.py
+++ geniusfeed/urls.py
@@ -2,15 +2,31 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 #Using url like models?
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 #Custom views from geniusfeed
-from geniusfeed import views
-
+from geniusfeed.views import FeedViewSet, UserViewSet, api_root
 # ViewSets define the view behavior.
 #class UserViewSet(viewsets.ModelViewSet):
 #    queryset = User.objects.all()
 #    serializer_class = UserSerializer
+
+feed_list = FeedViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+feed_detail = FeedViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -23,13 +39,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'geniusfeed.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^feeds/$', views.FeedList.as_view(), name='feed-list'),
-    url(r'^feeds/(?P<pk>[0-9]+)$', views.FeedDetails.as_view(), name='feed-details'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+    url(r'^feeds/$', feed_list, name='feed-list'),
+    url(r'^feeds/(?P<pk>[0-9]+)$', feed_detail, name='feed-details'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/',include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', views.api_root),
+    url(r'^', api_root),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- geniusfeed/views.py
+++ geniusfeed/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, generics
+from rest_framework import permissions, generics, viewsets
 from rest_framework.decorators import api_view, permission_classes
 from geniusfeed.models import Feed, FeedItem
 from geniusfeed.serializers import FeedSerializer, UserSerializer
@@ -19,30 +19,25 @@ def api_root(request, format=None):
 
 
 #@permission_classes((permissions.AllowAny,))
-class FeedList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+class FeedViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    """
     queryset = Feed.objects.all()
     serializer_class = FeedSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         feed = serializer.save()
         feed.users.add(self.request.user)
 
-
-#@permission_classes((permissions.AllowAny,))
-class FeedDetails(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly)
-    queryset = Feed.objects.all()
-    serializer_class = FeedSerializer
-
 @permission_classes((permissions.AllowAny,))
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-@permission_classes((permissions.AllowAny,))
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
