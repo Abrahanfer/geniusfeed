efc38d13e23157e11a9d5a09fcf911493c617321
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2015-08-06 11:12:55 +0000

    Change to Class-based views

--- geniusfeed/views.py
+++ geniusfeed/views.py
@@ -5,49 +5,52 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status, permissions
+from rest_framework.views import APIView
 from geniusfeed.models import Feed, FeedItem
 from geniusfeed.serializers import FeedSerializer
 
-@api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
-def feed_list(request, format=None):
+class FeedList(APIView):
     """
     List all feed, or create a new feed
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         feeds = Feed.objects.all()
         serializer = FeedSerializer(feeds, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = FeedSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes((permissions.AllowAny,))
-def feed_details(request, pk, format=None):
+class FeedDetails(APIView):
     """
     Return detail view for Feed
     """
-    try:
-        feed = Feed.objects.get(pk=pk)
-    except Feed.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    def get_object(self, pk):
+        try:
+            feed = Feed.objects.get(pk=pk)
+        except Feed.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        feed = self.get_object(pk)
         serializer = FeedSerializer(feed)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        feed = self.get_object(pk)
         serializer = FeedSerializer(feed, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        feed = self.get_object(pk)
         feed.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
