53e45ac802bb85279b79923a3382cb8c1cc4a66b
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2015-08-06 11:30:05 +0000

    Rewrite to use GenericsAPIView classes

--- geniusfeed/views.py
+++ geniusfeed/views.py
@@ -1,48 +1,15 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework import status, permissions, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import generics
 from geniusfeed.models import Feed, FeedItem
 from geniusfeed.serializers import FeedSerializer
 
 
 @permission_classes((permissions.AllowAny,))
-class FeedList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
-    """
-    List all feed, or create a new feed
-    """
+class FeedList(generics.ListCreateAPIView):
     queryset = Feed.objects.all()
     serializer_class = FeedSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
 @permission_classes((permissions.AllowAny,))
-class FeedDetails(mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  mixins.DestroyModelMixin,
-                  generics.GenericAPIView):
-    """
-    Return detail view for Feed
-    """
+class FeedDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Feed.objects.all()
     serializer_class = FeedSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.delete(request, *args, **kwargs)
