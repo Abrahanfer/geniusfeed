c3935f97f2d36cc6760ec12c08638de6aae36015
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2015-09-10 20:41:56 +0000

    Change format to improve readability

--- geniusfeed/models.py
+++ geniusfeed/models.py
@@ -3,14 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Feed(models.Model):
-    title = models.CharField(max_length=200, blank=True, default='Default Title Feed')
-    link = models.URLField(default='http://localhost')# max_length=200 by default
+    title = models.CharField(max_length=200, blank=True,
+                             default='Default Title Feed')
+    link = models.URLField(default='http://localhost')# max_length=200
+    # by default
     # Relationships
     users = models.ManyToManyField(User)
 
 class FeedItem(models.Model):
-    title = models.CharField(max_length=200, blank=True, default='Default Title Feed Item')
-    link = models.URLField(default='http://localhost')# max_length=200 by default
+    title = models.CharField(max_length=200, blank=True,
+                             default='Default Title Feed Item')
+    link = models.URLField(default='http://localhost')# max_length=200
+    # by Default
     # Relationship
     feed = models.ForeignKey(Feed, related_name='feed_items')
     users = models.ManyToManyField(User, through='FeedItemRead')
